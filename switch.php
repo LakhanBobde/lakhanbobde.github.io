a84134fb4881e78bd2b49dc56a526c09fcd06d10
LakhanBobde <45265451+LakhanBobde@users.noreply.github.com>
2018-11-30 01:08:26 +0530

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+  
+  $number=4;
+switch($number){  
+case 24:
+  echo"It is 24";
+  break;
+case 23:
+  echo"It is 23";
+  break;
+case 4:
+  echo"It is 4";
+  break;
+default:
+ echo"The number is not match";
+}
+?>
